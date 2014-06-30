431c49e86317dc43f3557727f964b0544852c190
Manoj Joshi <joshi.manoj@gmail.com>
2014-06-29 20:54:14 -0700

    java docs

---
+++ src/main/java/org/redbasin/lesson2/StringImmutability.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.redbasin.lesson2;
+
+/**
+ * String in Java is immutable. Meaning, we cannot change it's contents.
+ * The String s points to a location in memory called "Hello", when it is initialized to
+ * "World", it points to a new location in memory called "Wprld".
+ * The in-place modification of a String is not possible.
+ *
+ * @author redbasin
+ */
+public class StringImmutability {
+    public static void main(String[] args) {
+        String s = "Hello";
+        s = "World";
+    }
+}
