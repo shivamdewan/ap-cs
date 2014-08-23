a6defb05e4304c7b49d859196e235146ca3a820a
Tanisha Joshi <tanishajoshi.guitar@gmail.com>
2014-08-23 11:10:35 -0700

    problem 6

--- src/main/java/org/redbasin/classwork3/ProductEven6.java
+++ src/main/java/org/redbasin/classwork3/ProductEven6.java
@@ -7,24 +7,24 @@
 package org.redbasin.classwork3;
 
 /**
- * Compute the sum of odd numbers between 1 and 100, including 1.
+ * Compute the product of even numbers between 1 and 15.
  * 
  * It should print
  * 2500
  * 
  * @author mjoshi
  */
-public class SumOdd5 {
+public class ProductEven6 {
     
     public static void main(String[] args) {
         int min = 1;
         int max = 100;
-        int sum = 0;
+        int product = 1;
         for (int i = min; i <= max; i++) {
-            if (i % 2 == 1)
-                sum += i;
+            if (i % 2 == 0)
+                product *= i;
         }
-        System.out.println(sum);
+        System.out.println(product);
     }
     
 }
