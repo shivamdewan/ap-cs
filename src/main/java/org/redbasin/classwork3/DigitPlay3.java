2d9a8b556b35608f65a99aaa514955849d349034
Tanisha Joshi <tanishajoshi.guitar@gmail.com>
2014-08-23 09:51:18 -0700

    problem 3

---
+++ src/main/java/org/redbasin/classwork3/DigitPlay3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.redbasin.classwork3;
+
+/**
+ * Output
+ * 18 / 9 = 2
+ * @author mjoshi
+ */
+public class DigitPlay3 {
+    
+    public static void main(String args[]) {
+        int x = 18;
+        int y = 9;
+        // Correct way of doing it
+        System.out.println(x + " / " + y + " = " + (x/y));
+    }   
+}
