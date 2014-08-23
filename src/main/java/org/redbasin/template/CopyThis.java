751feb735186db68c2f2c02be43c1c154cb4f5e8
Tanisha Joshi <tanishajoshi.guitar@gmail.com>
2014-08-22 22:24:25 -0700

    adding template

---
+++ src/main/java/org/redbasin/template/CopyThis.java
@@ -0,0 +1,31 @@
+package org.redbasin.template;
+
+/**
+ * Copy this template for all your programs, to a fresh directory
+ * or package where you would like to develop your own code.
+ *
+ * 1. Change the name of the class "CopyThis" to the name you wish to have.
+ *    For instance MyPalindrome.
+ * 2. Make sure the file name is the same name, for instance MyPalindrome.java
+ * 3. Change the name of the package from org.redbasin.template to
+ *    for eg. org.redbasin.mystrings
+ * 4. Make sure you have consistent indentation.
+ * 5. Write your name here for the author field in below format.
+ *
+ * @author Doe, John (john.doe@example.com)
+ */
+public class CopyThis {
+
+  /**
+   * Every program you write must have the following main declared 
+   * exactly as below. Please do not change any of the words below such
+   * as "public", "static", "void", "main", "String[]".
+   *
+   * Every program must have a main.
+   *
+   * @param args These are the arguments passed to the program.
+   */
+  public static void main(String[] args) {
+  }
+
+}
