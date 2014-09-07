7a7526fe7f752e90d4ba8c86951222783f6dd5d2
Manoj Joshi <joshi.manoj@gmail.com>
2014-09-07 15:55:58 -0700

    added some more assignments

--- src/main/java/org/redbasin/labwork1/ReverseInputString.java
+++ src/main/java/org/redbasin/labwork1/ReverseInputString.java
@@ -1,17 +1,13 @@
 package org.redbasin.labwork1;
-
 import java.util.Scanner;
-
 /**
  * Reverse characters in the input String and print it.
- *
  * 1. Type this program and compile it.
  * 2. Run it and verify the behavior.
  * 3. Modify it so that it will ask "Enter a line: " each time before you type the line
  * 4. Modify it so that if you enter quit or QUIT, it will quit the program instead of reversing quit.
  * 5. Now modify it so that when you have reversed 5 strings it will say bye and exit.
  * 6. Reverse the string a second time to get back the original string and print it.
- *
  * @author mjoshi
  */
 public class ReverseInputString {
@@ -20,6 +16,7 @@ public class ReverseInputString {
         String input;
         while (in.hasNextLine()) {
            input = in.nextLine();
+           String s = "";
            for (int i = input.length()-1; i >= 0; i--) {
                System.out.print(input.charAt(i));
            }
