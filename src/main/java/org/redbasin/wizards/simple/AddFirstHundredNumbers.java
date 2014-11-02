2f68411c1709641c14c08d076c9c92c7dd6c20cc
Manoj Joshi <joshi.manoj@gmail.com>
2014-11-02 15:17:24 -0800

    Wrote initial code

--- src/main/java/org/redbasin/wizards/simple/AddFirstHundredNumbers.java
+++ src/main/java/org/redbasin/wizards/simple/AddFirstHundredNumbers.java
@@ -1,36 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.redbasin.wizards.simple;
-
-/**
- *
- * @author redbasin
- */
 public class AddFirstHundredNumbers {
-
     public static final int START_NUMBER = 1;
     public static final int STOP_NUMBER = 100;
-
     public static int addNumbers() {
         int counter = START_NUMBER;
         int sum = 0;
-
         while (counter <= STOP_NUMBER) {
            sum = sum + counter;
            counter = counter + 1;
         }
         return sum;
     }
-
     public static void main(String[] args) {
-
         int mySum = addNumbers();
-
         System.out.println("Correct Sum = " + mySum);
     }
-
 }
