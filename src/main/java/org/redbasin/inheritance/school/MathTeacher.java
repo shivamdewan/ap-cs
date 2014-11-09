d95cc3d4b9d860abb83264fe315b87fba80e64d7
Manoj Joshi <joshi.manoj@gmail.com>
2014-11-09 14:40:20 -0800

    Wrote initial code

--- src/main/java/org/redbasin/inheritance/school/MathTeacher.java
+++ src/main/java/org/redbasin/inheritance/school/MathTeacher.java
@@ -2,4 +2,10 @@ package org.redbasin.inheritance.school;
 
 public class MathTeacher extends Teacher {
   public String mathSubject;
+
+  public void printInfo() {
+    System.out.println("school = " + school);
+    System.out.println("subject = " + subject);
+    System.out.println("mathSubject = " + mathSubject);
+  }
 }
--- src/main/java/org/redbasin/inheritance/school/SchoolTest.java
+++ src/main/java/org/redbasin/inheritance/school/SchoolTest.java
@@ -6,5 +6,27 @@ public class SchoolTest {
       mteacher.mathSubject = "Calculus";
       mteacher.subject = "Math";
       mteacher.school = "EVHS";
+
+      Educator educator = (Educator)mteacher;
+      System.out.println("type of educator = " + educator.getClass().getName());
+      System.out.println("educator.school = " + educator.school);
+
+      Teacher teacher = (Teacher)mteacher;
+      System.out.println("type of teacher = " + teacher.getClass().getName());
+      System.out.println("teacher.school = " + teacher.school);
+      System.out.println("teacher.subject = " + teacher.subject);
+
+      MathTeacher m1teacher = (MathTeacher)teacher;
+      System.out.println("type of m1teacher = " + m1teacher.getClass().getName());
+      System.out.println("m1teacher.school = " + m1teacher.school);
+      System.out.println("m1teacher.subject = " + m1teacher.subject);
+      System.out.println("m1teacher.mathSubject = " + m1teacher.mathSubject);
+
+      Teacher teacher1 = new Teacher();
+      teacher1.school = "EVHS";
+      MathTeacher mteacher1 = (MathTeacher)teacher1;   // now allowed
+      System.out.println("type of mteacher1 = " + mteacher1.getClass().getName());
+      mteacher1.mathSubject = "Precalculus";
+      System.out.println("mteacher1.mathSubject = " + mteacher1.mathSubject);
    }
 }
