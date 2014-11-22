e68548218370ac1cbd820bf555f49276da59539b
Manoj Joshi <joshi.manoj@gmail.com>
2014-11-22 11:45:27 -0800

    Wrote initial code

---
+++ src/main/java/org/redbasin/classwork8/README.md
@@ -0,0 +1,10 @@
+Which of the following can be used to replace /* missing code */ so that
+advance will correctly update the time?
+
+(a) minutes = minutes % 60;
+(b) minutes = minutes + hours % 60;
+(c) hours = hours + minutes / 60;
+     minutes = minutes % 60;
+(d) hours = hours + minutes % 60;
+     minutes = minutes / 60;
+(e) hours = hours + minutes / 60;
---
+++ src/main/java/org/redbasin/classwork8/TimeRecord.java
@@ -0,0 +1,45 @@
+package org.redbasin.classwork8;
+
+public class TimeRecord {
+    private int hours;
+    private int minutes;  // 0 <= minutes < 60
+    /** Constructs a TimeRecord object
+     * @param h the number of hours
+     *        Precondition: h >= 0
+     * @param m the number of minutes
+     *        Precondition: 0 <= m < 60
+     */
+    public TimeRecord(int h, int m) {
+        hours = h;
+        minutes = m;
+    }
+
+     /** @return the number of hours
+      */
+    public int getHours() {
+        // implementation now shown
+    }
+
+     /** @return the number of minutes
+      * Postcondition: 0 <= minutes < 60
+      */
+    public int getMinutes() {
+        // implementation not shown
+    }
+
+     /** Adds h hours and m minutes to this TimeRecord.
+      * @param h the number of hours
+      * Precondition: h >= 0
+      * @param m the number of minutes
+      * Precondition: m >= 0
+      */
+      public void advance(int h, int m) {
+           hours = hours + h;
+           minutes = minutes + m;
+           /*
+             missing code
+           */
+      }
+// Other methods not shown
+
+}
